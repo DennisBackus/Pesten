24c9bad8d44ef9f460594e4e0fe5fc9e6c46217c
Dennis Backus <dennisbackus@hotmail.com>
2018-08-29 15:30:18 +0200

    Werkend pesten zonder pestkaarten, 2 spelers

--- Spelers.java
+++ Spelers.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 
 public class Spelers {
 	ArrayList<Kaart> hand  = new ArrayList<>(); 
+	String spelerNaam;
 	public Spelers() {
-	
+
 	}
 	
 
 	public void handBekijken()	{
-for (int a = 0; a <hand.size(); a++) {
-	System.out.println(hand.get(a).getFullCard_name());
+for (int a = 0; a < hand.size(); a++) {
+	System.out.println((a+1) + ". " + hand.get(a).getFullCard_name());
 }}
 	
 }
